20f793cb7d3de6738310b437a18085c73b1af2f9
SebastianMelin <57399859+SebastianMelin@users.noreply.github.com>
2020-02-08 18:04:49 +0100

    Add files via upload

---
+++ kassörska (skärmprogrammet)/kassörska/databas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+namespace kassörska
+{
+    public class Databas
+    {
+
+        string ConnectionString = "Data Source=sql6009.site4now.net;Initial Catalog=DB_A53DDD_topdog;Persist Security Info=True;User ID=DB_A53DDD_topdog_admin;Password=falling-2apple";
+        SqlConnection con;
+
+
+        public void OpenConection()
+        {
+            con = new SqlConnection(ConnectionString);
+            con.Open();
+        }
+
+        public void CloseConnection()
+        {
+            con.Close();
+        }
+
+        public void ExecuteQueries(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, con);
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public SqlDataReader DataReader(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+       
+
+    }
+}
