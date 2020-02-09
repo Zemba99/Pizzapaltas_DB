88527a8fd43f2e0ece62533f44701a821034352d
SebastianMelin <57399859+SebastianMelin@users.noreply.github.com>
2020-02-09 22:27:44 +0100

    Add files via upload

---
+++ kassörska (bemmanad)/kassörska/Console.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Linq;
+
+
+namespace kassörska
+{
+    class Consolen
+    {
+
+
+        public static void ShowWindow()
+        {
+            // vad som ska hämtas från datorbasen
+            string val = "SELECT * FROM dbo.Orders WHERE Order_status = 1";
+            int number = 0;
+            
+            int poss = 1;
+            Databas a = new Databas();
+
+
+            // för att loopen alltid ska var igång
+            bool alltid = true;
+            
+
+            // loop så att consolen ska kunna uppdateras
+            while (alltid)
+            {
+
+
+                a.OpenConection();
+                SqlDataReader dr = a.DataReader(val);
+                int test = a.Tester();
+                Console.WriteLine("Ordar som ska hämtas: \n");
+                dr.Read();
+                while (dr.Read())
+                {
+                    
+                    
+
+                    Console.WriteLine(dr["ID"].ToString());
+
+
+
+                
+                     //set test as the last posible number
+                    test = number;
+                // go oute of loop
+                   dr.Close();
+                //if last vlue is it change all exlse to normal
+                Console.BackgroundColor = ConsoleColor.Black;
+                }
+                // if you press down or s
+                Console.ForegroundColor = ConsoleColor.Black;
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.DownArrow || key.KeyChar == 's')
+                {
+                    // poss gouse down one position in the text
+                    poss = poss + 1;
+                // and start going thru the oredrs
+                    dr.Read();
+                    //clear the window
+                    Console.Clear();
+                    //reset the number on orders
+                    number = 0;
+                }
+                // same as down but oppisit way
+                if (key.Key == ConsoleKey.UpArrow || key.KeyChar == 'w')
+                {
+                    poss = poss - 1;
+                    dr.Read();
+                    Console.Clear();
+                    number = 0;
+                }
+                    // if u press enter
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    // clear window
+                    Console.Clear();
+
+
+
+                    //set number to 0
+                    number = 0;
+                    // write oute the rite order
+
+
+
+                       foreach (var item in dr)
+                       {
+                        number++;
+                        if (key.Key == ConsoleKey.Enter &&  number == poss)
+                        {
+                            a.ExecuteQueries("DELETE FROM dbo.Orders WHERE COUNT()");
+                        }
+                        //when poss is number the write the item
+                        if (poss == number)
+                        {
+                            
+
+                            Console.WriteLine($"{item}");
+
+
+
+                        }
+                       }
+                    }
+                    else { }
+
+                //look if u go over or under the posible value go to lowest numberor highest
+                if (poss > test)
+                {
+                    poss = 1;
+                }
+                if (poss == 0)
+                {
+                    poss = test;
+                }
+                
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+        
+    
+                    
+                    
+                  
+
+                   
+                  
+                   
+                   
+                  
+                       
+                    
+                       
+                       
+                 
+
+                  
+                
+                
+                
+
+    
+
---
+++ kassörska (bemmanad)/kassörska/databas.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+namespace kassörska
+{
+    public class Databas
+    {
+
+        string ConnectionString = "Data Source=sql6009.site4now.net;Initial Catalog=DB_A53DDD_topdog;Persist Security Info=True;User ID=DB_A53DDD_topdog_admin;Password=falling-2apple";
+        SqlConnection con;
+
+
+        public void OpenConection()
+        {
+            con = new SqlConnection(ConnectionString);
+            con.Open();
+        }
+
+        public void CloseConnection()
+        {
+            con.Close();
+        }
+
+        public void ExecuteQueries(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, con);
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public SqlDataReader DataReader(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+        public int Tester()
+        {
+            string stmt = "SELECT COUNT(*) FROM dbo.Orders WHERE Order_status=1";
+            int count = 0;
+
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmdCount = new SqlCommand(stmt, con))
+                {
+                    con.Open();
+                    count = (int)cmdCount.ExecuteScalar();
+                }
+            }
+            return count;
+        }
+
+
+
+    }
+}
